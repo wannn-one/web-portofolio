a7f1fc48e0c9695b65a5f4ff26e252b608f2e54c
Ikhwanul Abiyu Dhiyya'ul Haq <ikhwanulabiyu@gmail.com>
2022-09-03 12:30:46 +0700

    added navbar and icons

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,33 @@
-
 //Link for github
 
-document.getElementById("projectButton").addEventListener("click", function(){
-    location.href = `https://github.com/wannn-one?tab=repositories`;
+document.getElementById("github").addEventListener("click", function(){
+    location.href = 'https://github.com/wannn-one';
 });
 
+//projects link
+
+document.getElementById("projectButton").addEventListener("click", function(){
+    location.href = `./index.html#projects`;
+});
+
+//insta link
+
+document.getElementById("insta").addEventListener("click", function(){
+    location.href = `https://www.instagram.com/ikhwanulabiyu/`;
+});
+
+//Navbar
+
+const navBar = document.querySelector("nav"),
+        menuBtns = document.querySelectorAll(".menu-icon"),
+        overlay = document.querySelector(".overlay");
+
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
+});
+
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("open");
+});
